f11b0b8771eb02acd638bd49c1594e9ace917d5e
giangthidua <125744426+giangadua06042001@users.noreply.github.com>
2023-10-26 14:52:09 +0700

    update treeMap

--- src/Concept.java
+++ src/Concept.java
@@ -1,6 +1,5 @@
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class Concept {
 
@@ -9,20 +8,31 @@ public class Concept {
         HashMap<Integer, Animal> hashMap = new HashMap<>();
         Animal cat = new Animal(1, "Cat", 10.2);
         Animal dog = new Animal(2, "Dog", 5.2);
-        Animal chicken = new Animal(2, "Chicken", 3.2);
+        Animal chicken = new Animal(55, "Chicken", 3.2);
         Animal snake = new Animal(3, "Snake", 100.2);
-        Animal animal = new Animal(4, "Animal", 1.2);
-
-        hashMap.put(cat.getId(),cat);
+        Animal animal = new Animal(104, "Animal", 1.2);
+        HashMap<Integer,Animal>animalHashMap=new LinkedHashMap<>();
         hashMap.put(dog.getId(),dog);
+        hashMap.put(cat.getId(),cat);
         hashMap.put(chicken.getId(),chicken);
         hashMap.put(snake.getId(),snake);
         hashMap.put(animal.getId(),animal);
-        hashMap.put(0,cat);
         for (Map.Entry<Integer, Animal> entry : hashMap.entrySet()) {
             int key = entry.getKey();
             Animal value = entry.getValue();
             System.out.println("Key: " + key + ", Value: " + value);
+        }
+        System.out.println("================================================");
+        SortedMap<Integer,Animal>animalSortedMap=new TreeMap<>();
+        animalSortedMap.put(animal.getId(),animal);
+        animalSortedMap.put(dog.getId(),animal);
+        animalSortedMap.put(snake.getId(),snake);
+        animalSortedMap.put(cat.getId(),cat );
+        animalSortedMap.put(chicken.getId(),chicken);
+        for (Map.Entry<Integer,Animal>entry:animalSortedMap.entrySet()) {
+            int key=entry.getKey();
+            Animal animalTreeMap=entry.getValue();
+            System.out.println("Key:"+key +",Value:"+animalTreeMap);
         }
     }
 
